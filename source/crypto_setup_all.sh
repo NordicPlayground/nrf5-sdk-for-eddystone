dacebbfe8d70ab97704030fd2942291818884c2d
Ketil Erichsen <Ketil.Erichsen@nordicsemi.no>
2016-04-15 11:21:23 +0200

    fixes

---
+++ source/crypto_setup_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "Cloning Repositories..."
+git clone "https://github.com/massar/rfc6234.git" "crypto_libs/temp/rfc6234"
+git clone "https://github.com/ctz/cifra.git" "crypto_libs/temp/cifra"
+git clone "https://weave.googlesource.com/weave/libuweave" "crypto_libs/temp/tiny-aes128"
+
+echo "Creating directories..."
+mkdir crypto_libs/rfc6234
+mkdir crypto_libs/cifra
+
+echo "Moving files and folders..."
+
+# tiny-aes128
+mv crypto_libs/temp/tiny-aes128/third_party/tiny-aes128-c crypto_libs
+
+# cifra
+mv crypto_libs/temp/cifra/src/ext/handy.h crypto_libs/cifra
+mv crypto_libs/temp/cifra/src/*.h crypto_libs/cifra
+mv crypto_libs/temp/cifra/src/*.c crypto_libs/cifra
+
+# rfc6234
+mv crypto_libs/temp/rfc6234/*.txt crypto_libs/rfc6234
+mv crypto_libs/temp/rfc6234/*.c crypto_libs/rfc6234
+mv crypto_libs/temp/rfc6234/*.h crypto_libs/rfc6234
+
+echo "Cleaning up..."
+rm -rf crypto_libs/temp
+
+echo "Finished successfully"
